ee85b8bbeaf2c29b6536cff88c6348dbc2a79193
Peter Zaitsev <pz@percona.com>
2022-07-08 00:52:06 +0000

    minor fix

--- 0-7-sysbench-run.sh
+++ 0-7-sysbench-run.sh
@@ -1,1 +1,1 @@
-sysbench --db-driver=mysql --threads=8 --mysql-host=$NODE_IP --mysql-port=$MYSQL_PORT --mysql-user=root --mysql-password=$MYSQL_PASSWORD --mysql-db=sbtest /usr/share/sysbench/oltp_point_select.lua --report-interval=1 --table-size=1000000 --report-interval=1 --time=60 run
+sysbench --db-driver=mysql --threads=8 --mysql-host=$MYSQL_HOST --mysql-port=$MYSQL_TCP_PORT --mysql-user=root --mysql-password=$MYSQL_PWD --mysql-db=sbtest /usr/share/sysbench/oltp_point_select.lua --report-interval=1 --table-size=1000000 --report-interval=1 --time=60 run
